55f58e9b0c2dd3c615bedaf0b0b205f7c17f87a2
Alex Alday <alex.alday@tresta.com>
2022-01-16 17:31:46 -0500

    update deploy-commands to build all commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,13 +3,11 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, botToken } = require('./config.json');
 
-const commands = [];
-const commandFilesMisc = fs.readdirSync('./commands/miscellaneous').filter(file => file.endsWith('.js'));
-
-for (const file of commandFilesMisc) {
-    const command = require(`./commands/miscellaneous/${file}`);
-    commands.push(command.data.toJSON());
-}
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = commandFiles.map((file) => {
+    const command = require(`./commands/${file}`);
+    return command.data.toJSON();
+});
 
 const rest = new REST({ version: '9' }).setToken(botToken);
 
@@ -19,7 +17,8 @@ const rest = new REST({ version: '9' }).setToken(botToken);
             Routes.applicationGuildCommands(clientId, guildId),
             { body: commands },
         );
-    } catch (error) {
+    }
+ catch (error) {
         console.error(error);
     }
 })();
