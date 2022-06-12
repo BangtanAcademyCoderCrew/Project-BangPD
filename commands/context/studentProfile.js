8fd8f0d015edc2a6a4ecb1195f15544c3bb32248
Mariana <marianagaldomartinez@gmail.com>
2022-06-12 02:47:53 -0300

    added context menu command for studentProfile

--- commands/context/studentProfile.js
+++ commands/context/studentProfile.js
@@ -1,25 +1,25 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { ContextMenuCommandBuilder } = require('@discordjs/builders');
+const { ApplicationCommandType } = require('discord-api-types/v9')
 const { MessageEmbed } = require('discord.js');
-const { BATId, BALId, BADId } = require('../../config.json');
-const ALL_GUILD_IDS = [BATId, BALId, BADId];
+const { BATId, BALId, BAGId, BADId, guildId } = require('../../config.json');
+const ALL_GUILD_IDS = [BATId, BALId, BAGId, BADId, guildId];
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('studentprofile')
-    .setDescription('Gets info of a students across BA servers')
-    .addUserOption(option =>
-      option.setName('user')
-        .setDescription('The user you want to get their profile.')
-        .setRequired(true)),
+  data: new ContextMenuCommandBuilder()
+    .setName('profile')
+    .setType(ApplicationCommandType.User)
+    .setDefaultPermission(false),
   async execute(interaction) {
-    const options = interaction.options;
-    const user = options.getUser('user');
+
+    const userId = interaction.targetId;
     const fields = [];
+    const user = interaction.member.guild.members.cache.find(u => u.id === userId).user;
+
     await interaction.deferReply();
     ALL_GUILD_IDS.forEach(guildId => {
       const guild = interaction.client.guilds.cache.get(guildId);
       if (guildId && !guild) return;
-      const userInGuild = guild.members.cache.find(u => u.id === user.id);
+      const userInGuild = guild.members.cache.find(u => u.id === userId);
       if (!userInGuild) return;
       const roles = userInGuild.roles.cache.map(role => role.name).filter(role => role != '@everyone').join(', ');
       fields.push({
---
+++ commands/miscellaneous/getStudentProfile.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const { BATId, BALId, BADId, guildId } = require('../../config.json');
+const ALL_GUILD_IDS = [BATId, BALId, BADId, guildId];
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('studentprofile')
+    .setDescription('Gets info of a students across BA servers')
+    .setDefaultPermission(false)
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('The user you want to get their profile.')
+        .setRequired(true)),
+  async execute(interaction) {
+    const options = interaction.options;
+    const user = options.getUser('user');
+    const fields = [];
+    await interaction.deferReply();
+    ALL_GUILD_IDS.forEach(guildId => {
+      const guild = interaction.client.guilds.cache.get(guildId);
+      if (guildId && !guild) return;
+      const userInGuild = guild.members.cache.find(u => u.id === user.id);
+      if (!userInGuild) return;
+      const roles = userInGuild.roles.cache.map(role => role.name).filter(role => role != '@everyone').join(', ');
+      fields.push({
+        name: `Roles at ${guild.name}`,
+        value: roles,
+        inline: true
+      });
+    });
+    const createdAtDate = new Date(user.createdAt).toLocaleString('ko-KR', { timeZone: 'UTC' });
+    fields.push({
+      name: 'Created at',
+      value: `${createdAtDate} UTC`,
+      inline: false
+    });
+    const embed = new MessageEmbed()
+      .setColor('#5445ff')
+      .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
+      .addFields(fields)
+      .setTimestamp();
+    interaction.followUp({ embeds: [embed] });
+
+
+  }
+};
