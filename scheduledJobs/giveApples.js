d0af3a7e4bfe0c5e39ceff420a7740956553fe41
Alex Alday <alex.alday@tresta.com>
2022-10-02 17:31:25 -0400

    some embeds that still need to be made efficient 🙃

--- scheduledJobs/giveApples.js
+++ scheduledJobs/giveApples.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const Promise = require('promise');
 const { fetchAllMessagesByChannelSince } = require('../common/discordutil');
 const { DateTime } = require('luxon');
-const { MessageAttachment } = require('discord.js');
+const { EmbedBuilder, MessageAttachment } = require('discord.js');
 
 const greenAppleRoleId = '875740793078431825';
 const redAppleRoleId = '875740253980336158';
@@ -18,8 +18,8 @@ module.exports = {
         // running job at 02:00 at America/Chicago timezone
         cron.schedule('0 2 * * *', async () => {
             const cst = 'America/Chicago';
-            const currentDateTimeUTC = DateTime.utc().setZone(cst);
-            const messagesSinceDateTime = currentDateTimeUTC.minus({ days: 1 });
+            const currentDateTimeCT = DateTime.utc().setZone(cst);
+            const messagesSinceDateTime = currentDateTimeCT.minus({ days: 1 });
 
             const messagesWithApplesApplied = [];
             let logbookUserIds = [];
@@ -101,7 +101,6 @@ module.exports = {
             };
 
             const setGiveTheApples = async (channel) => {
-                // TODO: messagesSinceDateTime is Date type
                 const messages = await fetchAllMessagesByChannelSince(channel, messagesSinceDateTime);
                 const filteredMessages = filterOnlyValidMessages(messages);
                 return filteredMessages.map(message => {
@@ -119,24 +118,103 @@ module.exports = {
 
             // gather all logbook channels from every guild running BangPD
             const logbookChannels = client.channels.cache.filter(c => c.name.toLowerCase().includes(logbookChannelNameStem));
+            const jobDumpChannel = client.channels.cache.get(jobDumpChannelId);
+            if (!jobDumpChannel) {
+                console.log(`Scheduled Job: giveTheApples - no job dump channel with id ${jobDumpChannelId} found <a:shookysad:949689086665437184>`);
+            }
+
+            const startedEmbed = new EmbedBuilder()
+                .setColor('5445ff')
+                .setTitle('GiveTheApples - Started!')
+                .setDescription('I\'m currently checking in with Manager Sejin. Apples will be awarded shortly.')
+                .addFields(
+                    { name: 'Checking Logbooks From', value: `${messagesSinceDateTime.toLocaleString(DateTime.DATETIME_FULL)}`, inline: true },
+                    { name: 'Until', value: `${currentDateTimeCT.toLocaleString(DateTime.DATETIME_FULL)}`, inline: true }
+                );
+
+            if (jobDumpChannel) {
+                jobDumpChannel.send({ embeds: [startedEmbed] });
+            }
+
+            if (!logbookChannels && !logbookChannels.length > 0) {
+                console.log('Scheduled Job: giveTheApples - no logbook channels found <a:shookysad:949689086665437184>');
+                const completedNoLogDateTimeCT = DateTime.utc().setZone(cst);
+                const completedNoLogEmbed = new EmbedBuilder()
+                    .setColor('5445ff')
+                    .setTitle('GiveTheApples - Completed!')
+                    .setDescription('No logbook channels were found. Coder Crew assemble!')
+                    .addFields(
+                        { name: 'Completed at', value: `${completedNoLogDateTimeCT.toLocaleString(DateTime.DATETIME_FULL)}` }
+                    );
+                if (jobDumpChannel) {
+                    jobDumpChannel.send({ embeds: [completedNoLogEmbed] });
+                }
+            }
+
             if (logbookChannels && logbookChannels.length) {
                 await Promise.all(logbookChannels.map(async channel => {
                     await setGiveTheApples(channel);
                 }));
+
+                const chunkSize = 100;
+
+                // assign red apples, remove roll call and send update
                 await addAppleRoles();
+                const allRedApples = usersWithRedApple.split(/\r?\n/);
+                const redChunks = [];
+                for (let i = 0; i < Math.ceil(allRedApples.length / chunkSize); i++) {
+                    redChunks[i] = allRedApples.slice(i * chunkSize, (i + 1) * chunkSize);
+                }
+                redChunks.forEach((chunk, index) => {
+                    const redAppleEmbed = new EmbedBuilder()
+                        .setColor('5445ff')
+                        .setTitle(`GiveTheApples - Red Apples Assigned! ${index + 1} of ${redChunks.length}`)
+                        .setDescription(chunk);
+                    if (jobDumpChannel && index === redChunks.length - 1) {
+                        const attachmentRedApple = new MessageAttachment(Buffer.from(usersWithRedApple, 'utf-8'), 'usersID-redApple.txt');
+                        jobDumpChannel.send({ embeds: [redAppleEmbed], files: [attachmentRedApple] });
+                    } else {
+                        jobDumpChannel.send({ embeds: [redAppleEmbed] });
+                    }
+                });
+
+                // assign green apples, remove roll call and send update
                 await addGreenAppleRole();
+                const allGreenApples = usersWithGreenApple.split(/\r?\n/);
+                const greenChunks = [];
+                for (let i = 0; i < Math.ceil(allGreenApples.length / chunkSize); i++) {
+                    greenChunks[i] = allGreenApples.slice(i * chunkSize, (i + 1) * chunkSize);
+                }
+                greenChunks.forEach((chunk, index) => {
+                    const greenAppleEmbed = new EmbedBuilder()
+                        .setColor('5445ff')
+                        .setTitle(`GiveTheApples - Green Apples Assigned! ${index + 1} of ${greenChunks.length}`)
+                        .setDescription(chunk);
+                    if (jobDumpChannel && index === greenChunks.length - 1) {
+                        const attachmentGreenApple = new MessageAttachment(Buffer.from(usersWithGreenApple, 'utf-8'), 'usersID-redApple.txt');
+                        jobDumpChannel.send({ embeds: [greenAppleEmbed], files: [attachmentGreenApple] });
+                    } else {
+                        jobDumpChannel.send({ embeds: [greenAppleEmbed] });
+                    }
+                });
+
+                // mark logbooks as completed
                 await Promise.all(messagesWithApplesApplied.map(async (msg) => {
                     await msg.react('👍');
                 }));
-               const jobDumpChannel = client.channels.cache.get(jobDumpChannelId);
+
+                // well done bang pd nim, see you tomorrow!
+                const completedDateTimeCT = DateTime.utc().setZone(cst);
+                const completedEmbed = new EmbedBuilder()
+                    .setColor('5445ff')
+                    .setTitle('GiveTheApples - Completed!')
+                    .setDescription('All students from yesterday\'s logbooks have been awarded their apples. See you tomorrow!')
+                    .addFields(
+                        { name: 'Completed at', value: `${completedDateTimeCT.toLocaleString(DateTime.DATETIME_FULL)}` }
+                    );
                 if (jobDumpChannel) {
-                    const attachmentGreenApple = new MessageAttachment(Buffer.from(usersWithGreenApple, 'utf-8'), 'usersID-greenApple.txt');
-                    const attachmentRedApple = new MessageAttachment(Buffer.from(usersWithRedApple, 'utf-8'), 'usersID-redApple.txt');
-                    jobDumpChannel.send({ content: 'Scheduled Job: giveTheApples complete!', files: [attachmentGreenApple, attachmentRedApple] });
+                    jobDumpChannel.send({ embeds: [completedEmbed] });
                 }
-                console.log(`Scheduled Job: giveTheApples - no job dump channel with id ${jobDumpChannelId} found <a:shookysad:949689086665437184>`);
-            } else {
-                console.log('Scheduled Job: giveTheApples - no logbook channels found <a:shookysad:949689086665437184>');
             }
         }, {
             scheduled: true,
