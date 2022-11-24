2d2bfba4c4181acbcadde05e2064e62815fb8b3a
Mariana <marianagaldomartinez@gmail.com>
2022-11-24 10:05:43 -0300

    New command for buttons, library card button
    Added new command for creating buttons, added library card button and event listeners for new member joining satellite server and member updating passport roles

---
+++ buttons/renewLibraryCard.js
@@ -0,0 +1,30 @@
+const { guildId } = require("../config.json");
+const { redAppleRoleId, activeStudentRoleId, libraryCardRoleId, checkInRoleId } = require('./buttonsConfig.json');
+
+module.exports = {
+    name: 'renewLibraryCard',
+    /**
+     * 
+     * @param {ButtonInteraction} interaction 
+     * @param {GuildMember} member
+     */
+    async run(interaction, member) {
+        await interaction.deferReply({ephemeral: true });
+
+        if(member.roles.cache.has(libraryCardRoleId)) {
+            interaction.followUp({ content: "You already have your library card!", ephemeral: true })
+            return;
+        }
+
+        const guildMain = interaction.client.guilds.cache.get(guildId);
+        const memberInBA = guildMain.members.cache.find(m => m.id == interaction.member.id);
+
+        if(member.roles.cache.has(checkInRoleId) && (memberInBA.roles.cache.has(redAppleRoleId) ||  memberInBA.roles.cache.has(activeStudentRoleId))){
+            await member.roles.add([libraryCardRoleId]);
+            interaction.followUp({ content: "You got your library card renewed! 🎫", ephemeral: true })
+            return;
+        } 
+
+        interaction.followUp({ content: `Oops! You need the Check-In Role and the Active Student role to renew your card :pensive:`, ephemeral: true });
+    }
+}
