e64b50a5f4260609a0a50cf245fe4515e4877d3b
carmen <sonars.ficus-0b@icloud.com>
2023-03-05 13:10:14 -0500

    add student nickname to getStudentProfile embed output

--- commands/miscellaneous/getStudentProfile.js
+++ commands/miscellaneous/getStudentProfile.js
@@ -6,32 +6,33 @@ const ALL_GUILD_IDS = [BATId, BALId, BAGId, BADId, guildId];
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('getstudentprofile')
-    .setDescription('Gets info of a students across BA servers')
-    .setDefaultPermission(false)
-    .addUserOption(option =>
-      option.setName('user')
-        .setDescription('The user you want to get their profile.')
-        .setRequired(true)),
+      .setName('getstudentprofile')
+      .setDescription('Gets info of a students across BA servers')
+      .setDefaultPermission(false)
+      .addUserOption(option =>
+          option.setName('user')
+              .setDescription('The user you want to get their profile.')
+              .setRequired(true)),
   async execute(interaction) {
     const options = interaction.options;
     const user = options.getUser('user');
     const fields = [];
 
-    const formatStudentInfo = (guildName, value) => {
+    const formatField = (displayName, value) => {
       const info = {
-        name: `Roles at ${guildName}`,
+        name: `${displayName}: `,
         value: `${value}`,
-        inline: true
+        inline: false
       };
       return info;
     };
 
     await interaction.deferReply();
+    let userInGuild = null;
     ALL_GUILD_IDS.forEach(guildId => {
       const guild = interaction.client.guilds.cache.get(guildId);
       if (guildId && !guild) return;
-      const userInGuild = guild.members.cache.find(u => u.id === user.id);
+      userInGuild = guild.members.cache.find(u => u.id === user.id);
       if (!userInGuild) return;
       let roles = userInGuild.roles.cache.filter(role => role.name != '@everyone');
       if (interaction.guild == guildId) {
@@ -39,28 +40,26 @@ module.exports = {
       } else {
         roles = roles.map(role => role.name).join('\n');
       }
-      const joinedDate = new Date(userInGuild.joinedAt).toLocaleString('ko-KR', { timeZone: 'UTC' });
-      const result = `${roles}\n\n **Joined at:** ${joinedDate} UTC`;
-      if (result.length > 1024) {
-        const results = splitText(result, '\n');
-        results.forEach(result => {
-          fields.push(formatStudentInfo(guild.name, result));
+      if (roles.length > 1024) {
+        roles.forEach(role => {
+          fields.push(formatField(`Roles in ${guild.name}`, role));
         });
       } else {
-        fields.push(formatStudentInfo(guild.name, result));
+        fields.push(formatField(`Roles in ${guild.name}`, roles));
       }
     });
+    const displayName = userInGuild.displayName;
+    const joinedDate = new Date(userInGuild.joinedAt).toLocaleString('ko-KR', { timeZone: 'UTC' });
     const createdAtDate = new Date(user.createdAt).toLocaleString('ko-KR', { timeZone: 'UTC' });
-    fields.push({
-      name: 'Created at',
-      value: `${createdAtDate} UTC`,
-      inline: false
-    });
+
+    fields.unshift(formatField(`Display Name`, `${displayName}`));
+    fields.push(formatField(`Joined`, `${joinedDate} UTC`));
+    fields.push(formatField(`Created`, `${createdAtDate} UTC`));
     const embed = new MessageEmbed()
-      .setColor('#5445ff')
-      .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
-      .addFields(fields)
-      .setTimestamp();
+        .setColor('#5445ff')
+        .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
+        .addFields(fields)
+        .setTimestamp();
     interaction.followUp({ embeds: [embed] });
 
 
